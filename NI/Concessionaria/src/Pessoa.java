281e4aaaaba26f0cf6e617f2dd25bce62b3b9d07
Cassio Gama <157303477+Cassio-Gama@users.noreply.github.com>
2024-03-14 21:52:32 -0300

    Add files via upload

---
+++ NI/Concessionaria/src/Pessoa.java
@@ -0,0 +1,14 @@
+/*
+ 
+7. Uma pessoa pode ser associada a um veículo como proprietário.
+8. Crie uma classe chamada Pessoa com atributos como nome, CPF e
+   idade.
+
+ 
+*/
+public class Pessoa {
+
+	public String nome;
+	public int cpf, idade;
+	
+}
